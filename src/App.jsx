9ee250ef701a456a641e31b5b2966fd42100f8e2
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2024-10-23 22:40:23 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
-import React from 'react';
-import Bar from './components/barComponents/Bar';
-import Footer from './components/footerComponents/Footer';
+
 
 function App() {
   return (
