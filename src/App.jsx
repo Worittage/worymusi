856702d8895a85dc362c59fa1fc8c1a16eb1a0e7
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2024-10-23 22:39:50 +0200

    zu scheise

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Bar from './components/barComponents/Bar';
+import Footer from './components/footerComponents/Footer';
+
+function App() {
+  return (
+  <div></div>
+  );
+}
+
+export default App;
